2ae3278b4c943cf0bc9b6d098172d04f7121448a
vlzher <89138444+vlzher@users.noreply.github.com>
2022-06-24 14:15:27 +0200

    update longPulling

--- client/src/LongPulling.jsx
+++ client/src/LongPulling.jsx
@@ -11,22 +11,20 @@ const LongPulling = () => {
 
     const subscribe = async () => {
         try{
-            console.log("sub")
             const {data} = await axios.get('http://localhost:5000/get-messages')
             setMessages(prev =>[data, ...prev])
             await  subscribe()
 
         }
         catch (e){
-            console.log(9)
             setTimeout(async () => {
                 await subscribe()
-            }, 0)
+            }, 500)
+
         }
     }
 
     const sendMessage = async () => {
-        console.log("send")
         await axios.post('http://localhost:5000/new-messages',{
             message: value,
             id: Date.now()
